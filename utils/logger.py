9b416149a2267de74b9e061ab8febc0941562f42
Kailainy Santos <kailainys05@gmail.com>
2026-03-06 18:30:26 -0300

    refactor: modularize project structure into core, services, and config

---
+++ utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        
+        # Console handler
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        
+    return logger
